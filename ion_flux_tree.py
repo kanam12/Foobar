fbd1e77caabeb5dbe07646246636518f09587b65
Kana Mishra <kanam12@users.noreply.github.com>
2016-12-26 14:18:35 -0800

    final

--- ion_flux_tree.py
+++ ion_flux_tree.py
@@ -1,13 +1,13 @@
 def tree(h, q):
-	if h == 1:
+    if h == 1:
         return [-1]
     root = (2**h) - 1
     for i in range(len(q)):
-        end = 1
-        nodes = [(-1, root, end)]
+        leaf = 1
+        nodes = [(-1, root, leaf)]
         while nodes:
-            parent, curr, end = nodes.pop(0)
-            mid = (curr+end)//2
+            parent, curr, leaf = nodes.pop(0)
+            mid = (curr+leaf)//2
             if curr == q[i]:
                 q[i] = parent
                 break
@@ -15,6 +15,6 @@ def tree(h, q):
                 if curr - 1 > 0:
                     nodes = [(curr, curr - 1, mid)]
             else:
-                if range(mid, curr):
-                    nodes = [(curr, mid - 1, end)]
+                if curr - mid > 0:
+                    nodes = [(curr, mid - 1, leaf)]
     return q
